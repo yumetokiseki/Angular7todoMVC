e1b0b286e0d56dd680f2cb66bd56a52cd7bb14d3
T00064-RD3(林卓志) <T00064@senao.com.tw>
2018-11-10 15:07:14 +0800

    todo remove

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -31,4 +31,15 @@ export class AppComponent {
   allComplete() {
     this.todos.forEach(todo => (todo.isComplete = true));
   }
+
+  removeTodo(todo) {
+    // 方法1
+    // this.todos.splice(this.todos.indexOf(todo.lebel), 1);
+
+    // 方法2
+    this.todos = this.todos.filter(_todo => _todo !== todo);
+
+    // 方法3
+    // this.todos.splice(todo, 1);
+  }
 }
