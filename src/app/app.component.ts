8a0d87d9445984a561103b84189ad0bcadd7fb50
T00064-RD3(林卓志) <T00064@senao.com.tw>
2018-11-10 13:47:59 +0800

    ngif ngfor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,13 +6,21 @@ import { Component } from "@angular/core";
   styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  appTitle = "todo table";
+  appTitle = "todo";
   placeholder = "What needs to be done???";
   backgroundcolor = "#FF9999";
 
+  todos = [];
   todoValue;
 
   newTodo(event, inputElement) {
-    console.log(inputElement.value);
+    this.todos.push(inputElement.value);
+    inputElement.value = "";
+
+    //this.todos.push(todoValue.value);
+    //this.todoValue = "";
+
+    console.log(this.todos);
+    //console.log(inputElement.value);
   }
 }
